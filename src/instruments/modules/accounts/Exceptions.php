53aa172e722a6543923f0c6287464ac34e98cf73
Вадим Голубев <vadik.golubeff@yandex.ru>
2021-03-20 09:42:14 +0300

    Добавлена документация ядра, а также убраны стандартные модули из репозитория

--- src/instruments/modules/accounts/Exceptions.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-namespace modules\accounts;
-class AuthFailedException extends \Exception {}
-class AccountNotFoundException extends AuthFailedException {}
-class AccountExistsException extends AuthFailedException {}
-class NotEnoughAdminsException extends AuthFailedException {}
-class PermissionExistsException extends AuthFailedException {}
-class GroupNotFoundException extends AuthFailedException {}
-class AccountCryptException extends AuthFailedException {}
--- src/instruments/modules/templates/PageResourcesFront.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace modules\templates;
-class PageResourcesFront extends \PathExecutor {
-	public function validate() {
-		return false;
-	}
-	public function response(){}
-}
